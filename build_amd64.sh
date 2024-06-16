094e90163166259e027e1ac1dc2cc74a4a8c4a4b
go-while <go-while@users.noreply.github.com>
2024-06-16 17:13:51 +0200

    v0.0.2 alpha test release

--- build_amd64.sh
+++ build_amd64.sh
@@ -4,8 +4,10 @@ PATH="$PATH:/usr/local/go/bin"
 #export GOPATH=$(pwd)
 export GO111MODULE=auto
 #export GOEXPERIMENT=arenas
+rm -v ndbserver_amd64
 go build -trimpath -ldflags="-s -w" -o ndbserver_amd64 main.go
 RET=$?
+sha256sum ndbserver_amd64 > ndbserver_amd64.sha256sum
 echo $(date)
 test $RET -gt 0 && echo "BUILD FAILED! RET=$RET" || echo "BUILD OK!"
 exit $RET
--- client/build_amd64.sh
+++ client/build_amd64.sh
@@ -4,8 +4,10 @@ PATH="$PATH:/usr/local/go/bin"
 #export GOPATH=$(pwd)
 export GO111MODULE=auto
 #export GOEXPERIMENT=arenas
+rm -v client_amd64
 go build -trimpath -ldflags="-s -w" -o client_amd64 main.go
 RET=$?
+sha256sum client_amd64 > client_amd64.sha256sum
 echo $(date)
 test $RET -gt 0 && echo "BUILD FAILED! RET=$RET" || echo "BUILD OK!"
 exit $RET
