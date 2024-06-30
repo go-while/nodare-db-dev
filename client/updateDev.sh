850cc31365eb42823748988eeaa822aba3686a23
go-while <go-while@users.noreply.github.com>
2024-06-30 08:31:04 +0200

    Update updateDev.sh

--- client/updateDev.sh
+++ client/updateDev.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 test -z "$1" && echo "usage: $0 commit" && exit 1
 
-go get github.com/go-while/nodare-db-dev@$1
-go get github.com/go-while/nodare-db-dev/server@$1
-go get github.com/go-while/nodare-db-dev/logger@$1
+go get github.com/go-while/nodare-db-dev@$1 && \
+go get github.com/go-while/nodare-db-dev/server@$1 && \
+go get github.com/go-while/nodare-db-dev/logger@$1 && \
+go mod tidy && go mod vendor
